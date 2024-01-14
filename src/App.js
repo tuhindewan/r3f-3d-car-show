cba6e517399952b23cec8ec3691010133e9bcde8
Tuhin <tohin@coderex.co>
2024-01-14 14:05:31 +0600

    Prepare basic mesh on the canvas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import React, { Suspense } from 'react';
+import CarShow from './Components/CarShow';
+import './style.css';
 
 function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Suspense fallback={null}>
+            <Canvas shadows>
+                <CarShow/>
+            </Canvas>
+        </Suspense>
+    );
 }
 
 export default App;
