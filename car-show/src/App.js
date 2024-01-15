66b2496a3ca40df9188b9e13125a87f492cc871a
Tuhin <tohin@coderex.co>
2024-01-15 22:07:04 +0600

    Implements boxes on the scene

--- car-show/src/App.js
+++ car-show/src/App.js
@@ -11,6 +11,10 @@ import "./style.css";
 import { Ground } from "./Ground";
 import Car from "./Car";
 import { Rings } from "./Rings";
+import Boxes from "./Boxes";
+import { Bloom, ChromaticAberration, EffectComposer } from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
+import { FloatingGrid } from "./FloatingGrid";
 
 function CarShow() {
     return (
@@ -49,6 +53,23 @@ function CarShow() {
             )}
         </CubeCamera>
         <Rings />
+        <Boxes/>
+        <FloatingGrid />
+        <EffectComposer>
+            <Bloom
+                blendFunction={BlendFunction.ADD}
+                intensity={1.3}
+                width={300}
+                height={300}
+                kernelSize={5}
+                luminanceThreshold={0.15}
+                luminanceSmoothing={0.025}
+            />
+            <ChromaticAberration
+            blendFunction={BlendFunction.NORMAL} // blend mode
+            offset={[0.0005, 0.0012]} // color offset
+            />
+        </EffectComposer>
         </>
     );
 }
