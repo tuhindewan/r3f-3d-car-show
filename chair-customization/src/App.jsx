92737760ef0e1ebbbc92dcca9eca2c1d9c592114
Tuhin <tohin@coderex.co>
2024-01-17 00:04:20 +0600

    Project setup

---
+++ chair-customization/src/App.jsx
@@ -0,0 +1,18 @@
+import { Canvas } from '@react-three/fiber'
+import './App.css'
+import Experience from './components/Experience'
+
+function App() {
+
+    return (
+        <>
+            <div className='App'>
+                <Canvas>
+                    <Experience/>
+                </Canvas>
+            </div>
+        </>
+    )
+}
+
+export default App
