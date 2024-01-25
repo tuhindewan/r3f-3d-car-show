3da55e5eea2e0dc093391fd177bfab30f9c16322
Tuhin <tohin@coderex.co>
2024-01-25 23:01:37 +0600

    Prepare Navbar component

--- 3d-website/src/App.jsx
+++ 3d-website/src/App.jsx
@@ -1,13 +1,17 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import { Home, Contact, About, Projects } from "./pages"
 
 const App = () => {
     return (
         <main className="bg-slate-300/20">
             <Router>
-                Navbar
+                <Navbar/>
                 <Routes>
-                    <Route path="/" element={'Home'} />
-                    <Route path="/about" element={'About'} />
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/about" element={<About/>} />
+                    <Route path="/projects" element={<Projects/>} />
+                    <Route path="/contact" element={<Contact/>} />
                 </Routes>
             </Router>
         </main>
