eb8473466f5885e5e6270972d6695f0f2458d1ca
Tuhin <tohin@coderex.co>
2024-01-29 06:54:29 +0600

    Create Home component

--- 3d-website/src/pages/Home.jsx
+++ 3d-website/src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <section className="w-full h-screen relative">
+            <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+                POPUP
+            </div>
+        </section>
+    )
 }
 
 export default Home
